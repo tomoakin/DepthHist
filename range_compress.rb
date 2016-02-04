f7a7d634bc09cc7b9d089c0fd6934cdeb9609e04
Tomoaki NISHIYAMA <tomoakin@kenroku.kanazawa-u.ac.jp>
2016-02-04 16:25:52 +0900

    report minimum depth in each continual low coverage reagions

--- range_compress.rb
+++ range_compress.rb
@@ -2,12 +2,15 @@
 #
 lastref=nil
 lastpos=0
+min_depth=100000
 cur_range_start = nil
 
 ARGF.each_line do |l|
   a=l.chomp.split
   ref=a[0]
   pos=a[1].to_i
+  depth=a[2].to_i
+  min_depth = depth if depth < min_depth
   if lastpos == 0
     cur_range_start = pos
     lastref = ref
@@ -15,9 +18,10 @@ ARGF.each_line do |l|
     next
   end
   if ref!=lastref or pos != lastpos + 1
-    puts "#{lastref}:#{cur_range_start}-#{lastpos}"
+    puts "#{lastref}:#{cur_range_start}-#{lastpos}\t#{min_depth}"
     lastref=ref
     cur_range_start=pos
+    min_depth=100000
   end
   lastpos=pos
 end
